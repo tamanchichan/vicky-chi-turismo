188a1044d937d437ed3a4b19ab99216281bb5aad
tamanchichan <tamanchichan@gmail.com>
2025-10-25 05:48:58 -0500

    add generateStaticParams for locale-based routes

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,4 +1,9 @@
 import Header from "./components/Header";
+import { locales } from "../../config";
+
+export async function generateStaticParams() {
+    return locales.map((locale) => ({ lang: locale}));
+}
 
 export default async function LocaleLayout({ children, params }) {
     const { locale } = await params;
