d175ed8e50820e2b3205ee6790b543a0f517b4ac
tamanchichan <tamanchichan@gmail.com>
2025-10-31 03:57:36 -0500

    update tours rendereing, show only first 5 items

--- app/[locale]/components/ToursCatalogSnap.js
+++ app/[locale]/components/ToursCatalogSnap.js
@@ -1,4 +1,5 @@
 import { getDictionary } from "@/dictionary";
+import Link from "next/link";
 import styles from "./ToursCatalogSnap.module.css";
 
 export default async function ToursCatalog({ params }) {
@@ -7,15 +8,20 @@ export default async function ToursCatalog({ params }) {
     const tours = dictionary.ToursData;
     
     return (
-        <div className={`container flex ${styles.tours} snap`}>
-            {tours.map((tour, index) => (
-                <div className={styles.tour} key={index}>
+        <div className={`container flex ${styles["tours"]} snap`}>
+            {tours.slice(0, 5).map((tour, index) => (
+                <div className={`${styles["tour"]}`} key={index}>
                     <img src={tour.img} alt={tour.name}></img>
                     <h3>{tour.name}</h3>
                     <p>{tour.description}</p>
-                    <button className="button">{dictionary.ButtonComponent.details}</button>
+                    <button className="button">{dictionary.ButtonComponent.see_details}</button>
                 </div>
             ))}
+                <div className={`${styles["tour"]} ${styles["see-more"]}`}>
+                    <Link className={`button ${styles["see-more-button"]}`} href={`/${locale}/tours`}>
+                            {dictionary.ButtonComponent.see_more}
+                    </Link>
+                </div>
         </div>
     );
 };
